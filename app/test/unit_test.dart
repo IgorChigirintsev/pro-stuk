f244b76566309883e5c4b607f3aeb1a6b1c1fec0
Igor Chigirintsev <chigirintsevigor@gmail.com>
2026-08-11 00:54:44 +0500

    Английское дерево диагностики переведено полностью: 44 вопроса и 82 вердикта; версия 1.9.0

--- app/test/unit_test.dart
+++ app/test/unit_test.dart
@@ -176,14 +176,20 @@ void main() {
     LocaleService.current.value = 'ru';
   });
 
-  test('английские вопросы дерева переведены полностью', () async {
+  test('английское дерево переведено полностью: вопросы и вердикты', () async {
     LocaleService.current.value = 'en';
     final t = await DecisionTree.load();
     final cyrillic = RegExp(r'[А-Яа-яЁё]');
     final left = <String>[];
     for (final n in t.nodes.values) {
-      if (n.isLeaf) continue; // вердикты переводим следующим заходом
+
       if (cyrillic.hasMatch(n.text)) left.add(n.id);
+      if (cyrillic.hasMatch(n.topCause)) left.add('${n.id}/cause');
+      if (cyrillic.hasMatch(n.explanation)) left.add('${n.id}/why');
+      if (cyrillic.hasMatch(n.advice)) left.add('${n.id}/advice');
+      for (final a in n.altCauses) {
+        if (cyrillic.hasMatch(a)) left.add('${n.id}/alt');
+      }
       for (final o in n.options) {
         if (cyrillic.hasMatch(o.label)) left.add('${n.id}/${o.id}');
       }
